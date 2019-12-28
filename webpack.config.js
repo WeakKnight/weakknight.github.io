8c415a2416a74b40b76a55e78ea5c9d6f8dcf43d
Tianyu Li <ltyucb@gmail.com>
2019-12-27 23:58:09 -0700

    doc

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     devtool: 'inline-source-map',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'doc'),
     },
     module: {
         rules: [
