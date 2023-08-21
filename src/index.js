f836074c2067786621d3e03971fca80b1e250098
Tianyu Li <ltyucb@gmail.com>
2023-08-21 20:47:42 +0800

    Try marked katex extension.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ import './styles.css';
 import './highlight.css';
 
 const { marked } = require("marked");
+const markedKatex = require("marked-katex-extension");
+const options = {
+    throwOnError: false
+};
+
+marked.use(markedKatex(options));
+
 marked.setOptions({
     highlight: function (code, lang) {
         const language = hljs.getLanguage(lang) ? lang : 'plaintext';
@@ -17,48 +24,6 @@ marked.setOptions({
 });
 
 const renderer = new marked.Renderer();
-let originParagraph = renderer.paragraph.bind(renderer)
-renderer.paragraph = (text) => {
-    const blockRegex = /\$\$[^\$]*\$\$/g
-    const inlineRegex = /\$[^\$]*\$/g
-    let blockExprArray = text.match(blockRegex)
-    let inlineExprArray = text.match(inlineRegex)
-    for (let i in blockExprArray) {
-        const expr = blockExprArray[i]
-        const result = renderMathsExpression(expr)
-        text = text.replace(expr, result)
-    }
-    for (let i in inlineExprArray) {
-        const expr = inlineExprArray[i]
-        const result = renderMathsExpression(expr)
-        text = text.replace(expr, result)
-    }
-    return originParagraph(text)
-};
-
-function renderMathsExpression(expr) {
-    if (expr[0] === '$' && expr[expr.length - 1] === '$') {
-        let displayStyle = false
-        expr = expr.substr(1, expr.length - 2)
-        if (expr[0] === '$' && expr[expr.length - 1] === '$') {
-            displayStyle = true
-            expr = expr.substr(1, expr.length - 2)
-        }
-        let html = null
-        try {
-            html = katex.renderToString(expr)
-        } catch (e) {
-            console.err(e)
-        }
-        if (displayStyle && html) {
-            html = html.replace(/class="katex"/g, 'class="katex katex-block" style="display: block;"')
-        }
-        return html
-    } else {
-        return null
-    }
-}
-
 marked.setOptions({ renderer: renderer });
 
 const STATE_NONE = -1;
